f6d265e9eaf1edb9b66973c4e79627279b1ef9db
franco de gaetani <francodegaetani@gmail.com>
2025-09-11 16:23:44 -0300

    mi primer push

--- wp-content/themes/consultorio/templates/dega.php
+++ wp-content/themes/consultorio/templates/dega.php
@@ -6,6 +6,9 @@ get_header();
 ?>
 
 <h1>template dega</h1>
+<h2>segunto titulo</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sequi magnam est temporibus blanditiis omnis sunt laborum quod repellat commodi modi, minus quisquam porro possimus deserunt atque voluptate tenetur error!</p>
+
 
 <?php 
 get_footer();
