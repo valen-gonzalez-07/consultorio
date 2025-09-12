8cea3d04c91dbf51ef07bd561aec6a724bc36cbc
LucasBarragan <lucasbarragan000@gmail.com>
2025-09-12 20:23:08 -0300

    Primer estilado del form

--- wp-content/themes/consultorio/includes/queue.php
+++ wp-content/themes/consultorio/includes/queue.php
@@ -13,10 +13,13 @@ function enqueue_theme_styles() {
     // Register styles
     wp_register_style( 'google', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700', array(), null );
     wp_register_style( 'globals', get_template_directory_uri() . '/css/globals.css' );
-
+    wp_register_style( 'mainHeader', get_template_directory_uri() . '/css/components/mainHeader.css' );
+    wp_register_style( 'form', get_template_directory_uri() . '/css/components/form.css' );
     // enqueue styles
     wp_enqueue_style( 'google' );
     wp_enqueue_style( 'globals' );
+    wp_enqueue_style( 'mainHeader' );
+    wp_enqueue_style( 'form' );
     // Pages
     wp_register_style( 'error', get_template_directory_uri() . '/css/components/404.css' );
 
--- wp-content/themes/consultorio/templates/lucas.php
+++ wp-content/themes/consultorio/templates/lucas.php
@@ -7,7 +7,26 @@ get_header();
 
 <h1>template lucas</h1>
 
-<h2>Hola funciona?</h2>
+<section class= "form-container">
+    <div class= "iframe-div">
+        <iframe class= "form-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52989.569681330955!2d-60.60662903851542!3d-33.89356559624096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b84aad3ef60a31%3A0x481ac3277962e9c5!2sPergamino%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1757713996827!5m2!1ses-419!2sar" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class= "form-div">
+        <h2>Escribime</h2>
+        <form action="#" method="post">
+            <input class= "form-input" type="text" id="nombre" name="nombre" placeholder="Nombre y Apellido"><br><br>
+
+            <input class= "form-input" type="email" id="email" name="email" placeholder="Mail"><br><br>
+
+            <input class= "form-input" type="text" id="asunto" name="asunto" placeholder="Asunto"><br><br>
+
+            <textarea id="mensaje" name="mensaje" rows="6" placeholder="Escribí tu mensaje acá"></textarea><br><br>
+
+            <button class="form-button" type="submit">Enviar</button>
+        </form>
+    </div>
+</section>
+
 <?php 
 get_footer();
 ?>
