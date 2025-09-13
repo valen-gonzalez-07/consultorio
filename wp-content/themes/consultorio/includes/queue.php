35d0dc6c7d3c1e5ed86ac4accfd7e79d75c3b7fc
franco de gaetani <francodegaetani@gmail.com>
2025-09-13 19:16:38 -0300

    creando las section de hero-banner, especialidades y sobre-mi, con su css

--- wp-content/themes/consultorio/includes/queue.php
+++ wp-content/themes/consultorio/includes/queue.php
@@ -10,16 +10,31 @@ add_action( 'wp_print_styles', 'enqueue_theme_styles' );
  */
 
 function enqueue_theme_styles() {
-    // Register styles
+
+    // Register styles Fonts
     wp_register_style( 'google', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700', array(), null );
+    wp_register_style( 'inter', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap', array(), null );
+    wp_register_style( 'lora', 'https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap', array(), null );
+
+    // Register styles Componentes y Globals
+    wp_register_style( 'quien-soy', get_template_directory_uri() . '/css/components/quien-soy.css' );
+    wp_register_style( 'especialidades', get_template_directory_uri() . '/css/components/especialidades.css' );
+    wp_register_style( 'hero-banner', get_template_directory_uri() . '/css/components/hero-banner.css' );
     wp_register_style( 'globals', get_template_directory_uri() . '/css/globals.css' );
-    wp_register_style( 'mainHeader', get_template_directory_uri() . '/css/components/mainHeader.css' );
-    wp_register_style( 'form', get_template_directory_uri() . '/css/components/form.css' );
+
     // enqueue styles
+    //Fonts
     wp_enqueue_style( 'google' );
+    wp_enqueue_style( 'inter' );
+    wp_enqueue_style( 'lora' );
+
+    //Estilos globales y de Componentes
+    wp_enqueue_style( 'quien-soy' );
+    wp_enqueue_style( 'especialidades' );
+    wp_enqueue_style( 'hero-banner' );
     wp_enqueue_style( 'globals' );
-    wp_enqueue_style( 'mainHeader' );
-    wp_enqueue_style( 'form' );
+
+    
     // Pages
     wp_register_style( 'error', get_template_directory_uri() . '/css/components/404.css' );
 
