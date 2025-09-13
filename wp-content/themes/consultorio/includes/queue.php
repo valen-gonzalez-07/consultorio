be111211a69a9fba7dedffb77fea015cb5e25ab6
LucasBarragan <lucasbarragan000@gmail.com>
2025-09-13 19:22:48 -0300

    prueba de pull

--- wp-content/themes/consultorio/includes/queue.php
+++ wp-content/themes/consultorio/includes/queue.php
@@ -15,11 +15,28 @@ function enqueue_theme_styles() {
     wp_register_style( 'globals', get_template_directory_uri() . '/css/globals.css' );
     wp_register_style( 'mainHeader', get_template_directory_uri() . '/css/components/mainHeader.css' );
     wp_register_style( 'form', get_template_directory_uri() . '/css/components/form.css' );
+        // Register styles Fonts
+    wp_register_style( 'inter', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap', array(), null );
+    wp_register_style( 'lora', 'https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap', array(), null );
+        // Register styles Componentes y Globals
+    wp_register_style( 'quien-soy', get_template_directory_uri() . '/css/components/quien-soy.css' );
+    wp_register_style( 'especialidades', get_template_directory_uri() . '/css/components/especialidades.css' );
+    wp_register_style( 'hero-banner', get_template_directory_uri() . '/css/components/hero-banner.css' );
     // enqueue styles
-    wp_enqueue_style( 'google' );
+    
+    //Estilos globales y de Componentes
+    wp_enqueue_style( 'quien-soy' );
+    wp_enqueue_style( 'especialidades' );
+    wp_enqueue_style( 'hero-banner' );
     wp_enqueue_style( 'globals' );
+    wp_enqueue_style( 'google' );
     wp_enqueue_style( 'mainHeader' );
     wp_enqueue_style( 'form' );
+        // enqueue styles
+    //Fonts
+    wp_enqueue_style( 'inter' );
+    wp_enqueue_style( 'lora' );
+
     // Pages
     wp_register_style( 'error', get_template_directory_uri() . '/css/components/404.css' );
 
